f651a80aca107df6ed6849beebaa281c0a2789e6
Delfio Francisco <delfio_eu@hotmail.com>
2021-07-24 22:14:04 -0400

    criando página de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import Input from "../components/Form/Input";
 
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>dash go</h1>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW="360px"
+        bg="gray.800"
+        p={8}
+        rounded={8}
+        direction="column"
+      >
+        <Stack spacing={4}>
+          <Input label="Email" name="email" type="email" />
+          <Input label="Senha" name="password" type="password" />
+        </Stack>
+
+        <Button type="submit" mt={6} colorScheme="orange" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
