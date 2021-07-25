1d84b441937a8cafbc4bb24d5130ccba3966c136
Delfio Francisco <delfio_eu@hotmail.com>
2021-07-25 13:33:30 -0400

    Formulário de inclusão de usuário

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,68 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  SimpleGrid,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import {
+  RiAddBoxLine,
+  RiCloseCircleLine
+} from "react-icons/ri";
+import Input from "../../components/Form/Input";
+import Header from "../../components/Header";
+import SideBar from "../../components/SideBar";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <SideBar />
+
+        <Box flex="1" radioGroup="8" bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Adicionar usuário
+          </Heading>
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Nome completo" type="text" />
+              <Input name="email" label="E-mail" type="email" />
+            </SimpleGrid>
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" label="Senha" type="password" />
+              <Input
+                name="passwordConfirmation"
+                label="Confirmação da senha"
+                type="password"
+              />
+            </SimpleGrid>
+          </VStack>
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button
+                colorScheme="whiteAlpha"
+                rightIcon={<Icon as={RiCloseCircleLine} fontSize="20" />}
+              >
+                Cancelar
+              </Button>
+              <Button
+                colorScheme="orange"
+                rightIcon={<Icon as={RiAddBoxLine} fontSize="20" />}
+              >
+                Salvar
+              </Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
