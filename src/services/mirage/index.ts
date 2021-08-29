49948fda2dcdcc7e10fe36b83126bb5cb3a62993
Delfio Francisco <delfio_eu@hotmail.com>
2021-08-29 16:07:20 -0400

    Configurando factory dos dados no mirage

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,5 @@
-import { createServer, Model } from "miragejs";
+import { createServer, Factory, Model } from "miragejs";
+import faker from 'faker';
 
 type IUser = {
   name: string;
@@ -11,17 +12,36 @@ export function makeServer() {
     models: {
       user: Model.extend<Partial<IUser>>({}),
     },
+
+    factories: {
+      user: Factory.extend({
+        name() {
+            return faker.name
+        },
+        email() {
+            return faker.internet.email().toLowerCase()
+        },
+        created_at() {
+            return faker.date.recent(10, new Date())
+        },
+      }),
+    },
+
+    seeds(server) {
+        server.createList('user', 200);
+    },
+
     routes() {
-        this.namespace = 'api';
-        this.timing = 2000;
+      this.namespace = "api";
+      this.timing = 2000;
 
-        this.get('/users');
-        this.post('/users');
+      this.get("/users");
+      this.post("/users");
 
-        this.namespace = "";
-        this.passthrough();
+      this.namespace = "";
+      this.passthrough();
     },
   });
 
   return server;
-};
+}
